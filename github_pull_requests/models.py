04bfe7ad99a067aaca565e415168ecd711e5eeb4
fareedrecursolabs <fareed.zafar@recursolabs.com>
2025-03-21 14:34:13 +0500

    Pull request migrations correction

--- github_pull_requests/models.py
+++ github_pull_requests/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from github_repos.models import GithubRepo
 import uuid
-# Create your models here.
-
 
 class PullRequest(models.Model):
   id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
